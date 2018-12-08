0f2e96b347f75476af24c0cf51797254aeabc070
buames <jamesmakeseyes@gmail.com>
2018-12-08 12:20:14 -0800

    update deps

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,7 @@
-import path from 'path'
 import babel from 'rollup-plugin-babel'
 import resolve from 'rollup-plugin-node-resolve'
 import { uglify } from 'rollup-plugin-uglify'
-
-const pkg = require(path.resolve(process.cwd(), './package.json')) // eslint-disable-line
+import pkg from './package.json'
 
 const config = {
   input: './src/index.js',
