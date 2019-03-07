6c636f3320ab276980f8c3ab5d1f4acdd9fa864a
buames <jamesmakeseyes@gmail.com>
2019-03-07 09:46:29 -0800

    fix types
    move variants into an array
    
    more test
    
    readme

--- src/types.ts
+++ src/types.ts
@@ -1,45 +1,57 @@
-export enum LetterCasing {
-  Normal = 'normalcase',
-  Upper = 'uppercase',
-  Lower = 'lowercase'
+// tslint:disable max-line-length
+export type FontFamily = string;
+
+export type FontStyle = 'italic' | 'normal' | 'oblique' | string;
+
+export type FontWeight = 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900 | 'bold' | 'bolder' | 'normal' | 'lighter';
+
+export type LetterCasing = 'normalcase' | 'uppercase' | 'lowercase' | string;
+
+export type Property = string | 0 | number;
+
+export interface Typesettings {
+  family: FontFamily;
+  fallbacks?: FontFamily[];
+  variants?: Variant[];
 }
 
-export const enum FontStyle {
-  Normal = 'normal',
-  Italic = 'italic'
-}
-
-export type FontWeight = 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900
-
-export type Typesettings = {
-  family?: string;
-  fallback?: string;
-} & {
-  [key: string]: FontVariant;
-}
-
-export type FontVariant = {
+export interface Variant {
   fontWeight?: FontWeight;
   fontStyle?: FontStyle;
-  sources?: FileSources;
-} & {
-  [Casing in LetterCasing]?: { [n: number]: FontSettings };
-}
-
-export interface FileSources {
-  locals?: string[]
-  eot?: string
-  woff?: string
-  woff2?: string
-  ttf?: string
+  sources?: {
+    locals?: string[];
+    eot?: string;
+    woff?: string;
+    woff2?: string;
+    ttf?: string;
+  };
+  normalcase?: { [k: string]: FontSettings };
+  uppercase?: { [k: string]: FontSettings };
+  lowercase?: { [k: string]: FontSettings };
 }
 
 export interface FontSettings {
-  characterSpacing?: number
-  lineHeight?: number
-  paragraphSpacing?: number
+  characterSpacing?: Property;
+  lineHeight?: Property;
+  paragraphSpacing?: Property;
 }
 
 export interface AdditionalStyles {
-  [k: string]: string | number
+  [k: string]: Property;
+}
+
+export interface FontsResult {
+  [k: string]: {
+    fontFamily?: FontFamily,
+    fontStyle?: FontStyle,
+    fontSize?: Property,
+    fontWeight?: FontWeight,
+    letterSpacing?: Property,
+    lineHeight?: 'normal' | Property,
+    textTransform?: 'normal' | 'uppercase' | 'lowercase'
+  };
+}
+
+export interface GenerateFontsResult {
+  [size: string]: FontsResult;
 }
--- test/index.ts
+++ test/index.ts
@@ -1,68 +1,101 @@
-import test from 'ava'
-import { generateFonts, generateFontFace } from '../src'
+import test from 'ava';
+import { generateFonts, generateFontFace } from '../src';
+import { Typesettings } from '../src/types';
 
-const config = {
-  family: '\'Helvetica\'',
-  fallback: '-apple-system, BlinkMacSystemFont',
-  regular: {
-    fontStyle: 'normal',
-    fontWeight: 400,
-    sources: {
-      locals: ['Helvetica Regular', 'Helvetica-Regular']
+const config: Typesettings = {
+  family: 'Helvetica Neue',
+  fallbacks: ['-apple-system', 'BlinkMacSystemFont'],
+  variants: [
+    {
+      fontStyle: 'normal',
+      fontWeight: 400,
+      sources: {
+        locals: ['Helvetica Regular', 'Helvetica-Regular']
+      },
+      normalcase: {
+        12: {
+          characterSpacing: null,
+          lineHeight: 18,
+          paragraphSpacing: 0
+        }
+      }
     },
-    normalcase: {
-      12: {
-        characterSpacing: null,
-        lineHeight: 18,
-        paragraphSpacing: 0
+    {
+      fontStyle: 'normal',
+      fontWeight: 500,
+      sources: {
+        locals: ['Helvetica Medium', 'Helvetica-Medium'],
+        eot: './font-file.eot',
+        woff: './font-file.woff',
+        woff2: './font-file.woff2',
+        ttf: './font-file.ttf'
+      },
+      normalcase: {
+        20: {
+          characterSpacing: 0.29348573,
+          lineHeight: 22,
+          paragraphSpacing: 22
+        }
+      },
+      lowercase: {
+        20: {
+          characterSpacing: null,
+          lineHeight: null,
+          paragraphSpacing: null
+        }
+      },
+      uppercase: {
+        20: {
+          characterSpacing: 1,
+          lineHeight: 1,
+          paragraphSpacing: 1
+        }
+      }
+    },
+    {
+      fontStyle: 'normal',
+      fontWeight: 'bold',
+      sources: {
+        locals: ['Helvetica Medium', 'Helvetica-Medium'],
+        eot: './font-file.eot',
+        woff: './font-file.woff',
+        woff2: './font-file.woff2',
+        ttf: './font-file.ttf'
+      },
+      normalcase: {
+        20: {
+          characterSpacing: '0.29em',
+          lineHeight: 22,
+          paragraphSpacing: 22
+        }
       }
     }
-  },
-  medium: {
-    fontStyle: 'normal',
-    fontWeight: 500,
-    sources: {
-      locals: ['Helvetica Medium', 'Helvetica-Medium'],
-      eot: './font-file.eot',
-      woff: './font-file.woff',
-      woff2: './font-file.woff2',
-      ttf: './font-file.ttf'
-    },
-    normalcase: {
-      20: {
-        characterSpacing: 0.29348573,
-        lineHeight: 22,
-        paragraphSpacing: 22
-      }
-    },
-    lowercase: {
-      20: {
-        characterSpacing: null,
-        lineHeight: null,
-        paragraphSpacing: null
-      }
-    },
-    uppercase: {
-      20: {
-        characterSpacing: 1,
-        lineHeight: 1,
-        paragraphSpacing: 1
-      }
-    }
-  }
-}
+  ]
+};
 
 test('generateFontFace()', (t) => {
-  t.snapshot(generateFontFace(config as any))
-})
+  t.snapshot(generateFontFace(config), 'with fontFamily wrapped in quotes');
+
+  const a = { ...config, family: 'Helvetica' };
+  t.snapshot(generateFontFace(a), 'without fontFamily wrapped in quotes');
+});
+
+test('generateFontFace(): Should throw error when there are no file sources.', (t) => {
+  const error = t.throws(() => {
+    delete config.variants[0].sources;
+    return generateFontFace(config);
+  // tslint:disable-next-line: align
+  }, Error);
+
+  t.is(error.message, 'Missing font file sources');
+});
 
 test('generateFonts()', (t) => {
-  t.snapshot(generateFonts(config as any), 'with fallback family')
+  t.snapshot(generateFonts(config), 'with fallback family');
 
-  t.snapshot(generateFonts(config as any, {
-    textRendering: 'optimizeLegibility'
-  }),        'with additional styles')
+  // tslint:disable-next-line: max-line-length
+  t.snapshot(generateFonts(config, { textRendering: 'optimizeLegibility' }), 'with additional styles');
 
-  delete config.fallback
-  t.snapshot(generateFonts(config as any), 'without fallback family')
-})
+  delete config.fallbacks;
+  t.snapshot(generateFonts(config), 'without fallback family');
+});
