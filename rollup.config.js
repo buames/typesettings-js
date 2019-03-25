53611b1c97af0cb0d5a132565364d47a1f79356b
buames <james@postmates.com>
2019-03-24 19:38:17 -0700

    fix csstype, update readme

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import typescript from 'rollup-plugin-typescript2'
+import resolve from 'rollup-plugin-node-resolve'
 import { terser } from 'rollup-plugin-terser'
 import pkg from './package.json'
 
@@ -14,6 +15,7 @@ export default {
       useTsconfigDeclarationDir: true,
       cacheRoot: 'node_modules/.cache/rollup-ts'
     }),
+    resolve(),
     terser()
   ]
 }
