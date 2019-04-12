81884333220e8efc2798f1d69b9402cded8ff437
buames <jamesmakeseyes@gmail.com>
2019-04-12 07:59:07 -0700

    travis

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,15 @@
 /* eslint-disable import/no-extraneous-dependencies, global-require */
 import typescript from 'rollup-plugin-typescript2';
 import resolve from 'rollup-plugin-node-resolve';
+import sourceMaps from 'rollup-plugin-sourcemaps';
 import { terser } from 'rollup-plugin-terser';
 import pkg from './package.json';
 
 export default {
   input: 'src/index.ts',
   output: [
-    { file: pkg.main, format: 'cjs' },
-    { file: pkg.module, format: 'es' },
+    { file: pkg.main, format: 'cjs', sourcemap: true },
+    { file: pkg.module, format: 'es', sourcemap: true },
   ],
   plugins: [
     typescript({
@@ -17,6 +18,7 @@ export default {
       cacheRoot: 'node_modules/.cache/rollup-ts',
     }),
     resolve(),
+    sourceMaps(),
     terser(),
   ],
 };
